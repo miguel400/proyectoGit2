06f56867a25fe93e0d69998c1487f1adb054c605
miguel400 <141676109+miguel400@users.noreply.github.com>
2023-08-13 11:57:54 -0300

    Initial commit

---
+++ src/proyectogit2/alumno2/cliente.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogit2.alumno2;
+
+/**
+ *
+ * @author MIGUEL
+ */
+public class cliente {
+    
+}
